b7ff7c414d44f79c55672247476774dd6939d690
Przybylski 중광 <r351574nc3@gmail.com>
2012-07-26 08:34:09 -0700

    Preparing release

--- src/main/java/org/kualigan/maven/plugins/kfs/CreateOverlayMojo.java
+++ src/main/java/org/kualigan/maven/plugins/kfs/CreateOverlayMojo.java
@@ -167,7 +167,7 @@ public class CreateOverlayMojo extends AbstractMojo {
     @Parameter(property = "archetypeArtifactId", defaultValue = "kfs-archetype")
     protected String archetypeArtifactId;
     
-    @Parameter(property = "archetypeVersion", defaultValue = "1.0.4")
+    @Parameter(property = "archetypeVersion", defaultValue = "1.0.6-SNAPSHOT")
     protected String archetypeVersion;
     
     /**
