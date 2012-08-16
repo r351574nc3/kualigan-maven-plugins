ecce49fca92933d26ea048fa71878e9fc15c9172
Przybylski 중광 <r351574nc3@gmail.com>
2012-08-15 21:32:30 -0700

    Swiching to maven tooling

--- kfs-maven-plugin/src/main/java/org/kualigan/maven/plugins/kfs/CreatePrototypeMojo.java
+++ kfs-maven-plugin/src/main/java/org/kualigan/maven/plugins/kfs/CreatePrototypeMojo.java
@@ -66,7 +66,7 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+´∑import java.util.Map;
 import java.util.Properties;
 import java.util.StringTokenizer;
 
